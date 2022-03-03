2e16b580bab0258a2e439cc91f1dc17a90d937c1
karagozr <karagozr1@gmail.com>
2022-03-03 15:48:15 +0300

    Add project files.

---
+++ Helpers/DirectoryHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOODPEDI.API.REST.Helpers
+{
+    public static class DirectoryHelper
+    {
+        private static string _basePath = $"{AppContext.BaseDirectory}";
+
+        private static string _localData = $"{_basePath}Local Data\\";
+
+
+        public static string GetLocalDataPath(string folderName)
+        {
+            if (!Directory.Exists(_localData))  Directory.CreateDirectory(_localData);
+
+            if (!Directory.Exists(_localData + folderName)) Directory.CreateDirectory(_localData + folderName);
+
+            return _localData + folderName+"\\";
+
+        }
+
+        
+    }
+}
